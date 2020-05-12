403f0c4f325ff312f084721d9b283c0db5cdf000
MattiasNilsson <mattias@fuelcast.co>
2020-05-12 11:50:33 +0200

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Mattias Nilsson on 5/12/20.
+//  Copyright © 2020 Mattias Nilsson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
